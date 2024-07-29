4f0e964e8d2868421b51a4bd325cecec252e5c96
Yelhie <maelle.ew.m@gmail.com>
2024-07-29 18:58:27 +0200

    Test : Mise à jour des tests pour book model

--- backend/tests/models/book.model.test.ts
+++ backend/tests/models/book.model.test.ts
@@ -1,8 +1,8 @@
-import { ValidationError } from "sequelize";
-import Book from "../../src/models/book.models";
 import sequelize from "../../src/config/dbConfig";
+import Book from "../../src/models/book.model";
 
 beforeAll(async () => {
+  await sequelize.authenticate();
   await sequelize.sync({ force: true });
 });
 
@@ -10,275 +10,187 @@ afterAll(async () => {
   await sequelize.close();
 });
 
-type CreateFunction = () => Promise<any>;
-
-const assertValidationError = async (
-  createFn: CreateFunction,
-  expectedMessage: string
-) => {
-  try {
-    await createFn();
-  } catch (error) {
-    if (error instanceof ValidationError) {
-      expect(error).toBeInstanceOf(ValidationError);
-      expect(error.errors[0].message).toBe(expectedMessage);
-    } else {
-      throw error;
-    }
-  }
-};
-
-// Test model creation with valid attributes
 describe("Book Model", () => {
-  it("should create a book with valid attributes", async () => {
-    const newBook = {
+  it("should validate a book with all required fields", async () => {
+    const book = Book.build({
       title: "Test Title",
       author: "Test Author",
       tome: 1,
-      genre: "Test Genre",
-      price: 9.99,
+      price: 10.99,
       publishedDate: new Date(),
-      description: "Test Description",
-      popularity: 0,
-      stock: 0,
-      thumbnailImagePath: "/uploads/thumbail/test-image.jpg",
-      coverImagePath: "/uploads/cover/test-image.jpg",
-    };
-
-    const book = await Book.create(newBook);
-
-    expect(book.id).toBeDefined();
-
-    const { publishedDate, ...rest } = newBook;
-    expect(book).toMatchObject(rest);
-    expect(book.publishedDate.getTime()).toEqual(
-      newBook.publishedDate.getTime()
-    );
-  });
-
-  // Test model validation with invalid attributes title
-  it("should throw a validation error if title is missing", async () => {
-    await assertValidationError(
-      () =>
-        Book.create({
-          author: "Test Author",
-          tome: 1,
-          genre: "Test Genre",
-          price: 9.99,
-          publishedDate: new Date(),
-          description: "Test Description",
-          popularity: 0,
-          stock: 0,
-          thumbnailImagePath: "/uploads/thumbail/test-image.jpg",
-          coverImagePath: "/uploads/cover/test-image.jpg",
-        }),
-      "Le titre est obligatoire."
-    );
-  });
-
-  // Test model validation with invalid attributes author
-  it("should throw a validation error if author is missing", async () => {
-    await assertValidationError(
-      () =>
-        Book.create({
-          title: "Test Title",
-          tome: 1,
-          genre: "Test Genre",
-          price: 9.99,
-          publishedDate: new Date(),
-          description: "Test Description",
-          popularity: 0,
-          stock: 0,
-          thumbnailImagePath: "/uploads/thumbail/test-image.jpg",
-          coverImagePath: "/uploads/cover/test-image.jpg",
-        }),
-      "L'auteur est obligatoire."
-    );
-  });
-
-  // Test model validation with invalid attributes tome
-  it("should throw a validation error if tome is negative", async () => {
-    await assertValidationError(
-      () =>
-        Book.create({
-          title: "Test Title",
-          author: "Test Author",
-          tome: -1,
-          genre: "Test Genre",
-          price: 9.99,
-          publishedDate: new Date(),
-          description: "Test Description",
-          popularity: 0,
-          stock: 0,
-          thumbnailImagePath: "/uploads/thumbail/test-image.jpg",
-          coverImagePath: "/uploads/cover/test-image.jpg",
-        }),
-      "Le nombre de tome doit être supérieur ou égal à 1."
-    );
-  });
-
-  // Test model validation with invalid attributes genre
-  it("should throw a validation error if genre is missing", async () => {
-    await assertValidationError(
-      () =>
-        Book.create({
-          title: "Test Title",
-          author: "Test Author",
-          tome: 1,
-          price: 9.99,
-          publishedDate: new Date(),
-          description: "Short",
-          popularity: 0,
-          stock: 0,
-          thumbnailImagePath: "/uploads/thumbail/test-image.jpg",
-          coverImagePath: "/uploads/cover/test-image.jpg",
-        }),
-      "Le genre est obligatoire."
-    );
-  });
-
-  // Test model validation with invalid attributes price
-  it("should throw a validation error if price is negative", async () => {
-    await assertValidationError(
-      () =>
-        Book.create({
-          title: "Test Title",
-          author: "Test Author",
-          tome: 1,
-          genre: "Test Genre",
-          price: -1,
-          publishedDate: new Date(),
-          description: "Test Description",
-          popularity: 0,
-          stock: 0,
-          thumbnailImagePath: "/uploads/thumbail/test-image.jpg",
-          coverImagePath: "/uploads/cover/test-image.jpg",
-        }),
-      "Le prix doit être supérieur ou égal à 0."
-    );
-  });
-
-  // Test model validation with invalid attributes date
-  it("should have a default publishedDate if not provided", async () => {
-    const book = await Book.create({
-      title: "Test Title",
-      author: "Test Author",
-      tome: 1,
-      genre: "Test Genre",
-      price: 9.99,
-      description: "Test Description",
-      popularity: 0,
-      stock: 0,
+      description: "This is a valid description.",
+      popularity: 5,
+      stock: 10,
       thumbnailImagePath: "/uploads/thumbail/test-image.jpg",
       coverImagePath: "/uploads/cover/test-image.jpg",
     });
 
-    expect(book.publishedDate).toBeInstanceOf(Date);
-    expect(book.publishedDate.getTime()).toBeLessThanOrEqual(
-      new Date().getTime()
+    await expect(book.validate()).resolves.not.toThrow();
+  });
+
+  it("should throw a validation error if title is missing", async () => {
+    const book = Book.build({
+      title: "",
+      author: "Test Author",
+      tome: 1,
+      price: 10.99,
+      publishedDate: new Date(),
+      description: "This is a valid description.",
+      popularity: 5,
+      stock: 10,
+      thumbnailImagePath: "/uploads/thumbail/test-image.jpg",
+      coverImagePath: "/uploads/cover/test-image.jpg",
+    });
+
+    await expect(book.validate()).rejects.toThrow("Title cannot be empty.");
+  });
+
+  it("should throw a validation error if author is missing", async () => {
+    const book = Book.build({
+      title: "Test Title",
+      author: "",
+      tome: 1,
+      price: 10.99,
+      publishedDate: new Date(),
+      description: "This is a valid description.",
+      popularity: 5,
+      stock: 10,
+      thumbnailImagePath: "/uploads/thumbail/test-image.jpg",
+
+      coverImagePath: "/uploads/cover/test-image.jpg",
+    });
+
+    await expect(book.validate()).rejects.toThrow("Author cannot be empty.");
+  });
+
+  it("should throw a validation error if tome is negative", async () => {
+    const book = Book.build({
+      title: "Test Title",
+      author: "Test Author",
+      tome: -1,
+      price: 10.99,
+      publishedDate: new Date(),
+      description: "This is a valid description.",
+      popularity: 5,
+      stock: 10,
+      thumbnailImagePath: "/uploads/thumbail/test-image.jpg",
+      coverImagePath: "/uploads/cover/test-image.jpg",
+    });
+
+    await expect(book.validate()).rejects.toThrow(
+      "Number of tome must be greater than or equal to 1"
     );
   });
 
-  // Test model validation with invalid attributes description
-  it("should throw a validation error if description is too short", async () => {
-    await assertValidationError(
-      () =>
-        Book.create({
-          title: "Test Title",
-          author: "Test Author",
-          tome: 1,
-          genre: "Test Genre",
-          price: 9.99,
-          publishedDate: new Date(),
-          description: "Short",
-          popularity: 0,
-          stock: 0,
-          thumbnailImagePath: "/uploads/thumbail/test-image.jpg",
-          coverImagePath: "/uploads/cover/test-image.jpg",
-        }),
-      "La description doit contenir entre 10 et 500 caractères."
+  it("should throw a validation error if price is negative", async () => {
+    const book = Book.build({
+      title: "Test Title",
+      author: "Test Author",
+      tome: 1,
+      price: -1,
+      publishedDate: new Date(),
+      description: "This is a valid description.",
+      popularity: 5,
+      stock: 10,
+      thumbnailImagePath: "/uploads/thumbail/test-image.jpg",
+      coverImagePath: "/uploads/cover/test-image.jpg",
+    });
+
+    await expect(book.validate()).rejects.toThrow(
+      "Price must be greater than or equal to 0"
     );
   });
 
-  // Test model validation with invalid attributes popularity
-  it("should have a validation error if popularity is negative", async () => {
-    await assertValidationError(
-      () =>
-        Book.create({
-          title: "Test Title",
-          author: "Test Author",
-          tome: 1,
-          genre: "Test Genre",
-          price: 9.99,
-          publishedDate: new Date(),
-          description: "Test Description",
-          popularity: -1,
-          stock: 0,
-          thumbnailImagePath: "/uploads/thumbail/test-image.jpg",
-          coverImagePath: "/uploads/cover/test-image.jpg",
-        }),
-      "La popularité doit être supérieure ou égale à 0."
+  it("should throw a validation error if description is missing", async () => {
+    const book = Book.build({
+      title: "Test Title",
+      author: "Test Author",
+      tome: 1,
+      price: 10.99,
+      publishedDate: new Date(),
+      description: "short",
+      popularity: 5,
+      stock: 10,
+      thumbnailImagePath: "/uploads/thumbail/test-image.jpg",
+      coverImagePath: "/uploads/cover/test-image.jpg",
+    });
+
+    await expect(book.validate()).rejects.toThrow(
+      "Description must be between 10 and 500 characters."
     );
   });
 
-  // Test model validation with invalid attributes stock
-  it("should have a validation error if stock is negative", async () => {
-    await assertValidationError(
-      () =>
-        Book.create({
-          title: "Test Title",
-          author: "Test Author",
-          tome: 1,
-          genre: "Test Genre",
-          price: 9.99,
-          publishedDate: new Date(),
-          description: "Test Description",
-          popularity: 0,
-          stock: -1,
-          thumbnailImagePath: "/uploads/thumbail/test-image.jpg",
-          coverImagePath: "/uploads/cover/test-image.jpg",
-        }),
-      "Le stock doit être supérieur ou égal à 0."
+  it("should throw a validation error if popularity is negative", async () => {
+    const book = Book.build({
+      title: "Test Title",
+      author: "Test Author",
+      tome: 1,
+      price: 10.99,
+      publishedDate: new Date(),
+      description: "This is a valid description.",
+      popularity: -1,
+      stock: 10,
+      thumbnailImagePath: "/uploads/thumbail/test-image.jpg",
+      coverImagePath: "/uploads/cover/test-image.jpg",
+    });
+
+    await expect(book.validate()).rejects.toThrow();
+  });
+
+  it("should throw a validation error if stock is negative", async () => {
+    const book = Book.build({
+      title: "Test Title",
+      author: "Test Author",
+      tome: 1,
+      price: 10.99,
+      publishedDate: new Date(),
+      description: "This is a valid description.",
+      popularity: 5,
+      stock: -1,
+      thumbnailImagePath: "/uploads/thumbail/test-image.jpg",
+      coverImagePath: "/uploads/cover/test-image.jpg",
+    });
+
+    await expect(book.validate()).rejects.toThrow(
+      "Stock must be greater than or equal to 0."
     );
   });
 
-  // Test model validation with invalid attributes coverImagePath
-  it("should throw a validation error if coverImagePath is missing", async () => {
-    await assertValidationError(
-      () =>
-        Book.create({
-          title: "Test Title",
-          author: "Test Author",
-          tome: 1,
-          genre: "Test Genre",
-          price: 9.99,
-          publishedDate: new Date(),
-          description: "Test Description",
-          popularity: 0,
-          stock: 0,
-          thumbnailImagePath: "/uploads/thumbail/test-image.jpg",
-        }),
-      "Le chemin de l'image de couverture est obligatoire."
-    );
-  });
-
-  // Test model validation with invalid attributes thumbnailImagePath
   it("should throw a validation error if thumbnailImagePath is missing", async () => {
-    await assertValidationError(
-      () =>
-        Book.create({
-          title: "Test Title",
-          author: "Test Author",
-          tome: 1,
-          genre: "Test Genre",
-          price: 9.99,
-          publishedDate: new Date(),
-          description: "Test Description",
-          popularity: 0,
-          stock: 0,
-          coverImagePath: "/uploads/cover/test-image.jpg",
-        }),
-      "Le chemin de la miniature est obligatoire."
+    const book = Book.build({
+      title: "Test Title",
+      author: "Test Author",
+      tome: 1,
+      price: 10.99,
+      publishedDate: new Date(),
+      description: "This is a valid description.",
+      popularity: 5,
+      stock: 10,
+      thumbnailImagePath: "",
+      coverImagePath: "/uploads/cover/test-image.jpg",
+    });
+
+    await expect(book.validate()).rejects.toThrow(
+      "Thumbnail image path cannot be empty."
+    );
+  });
+
+  it("should throw a validation error if coverImagePath is missing", async () => {
+    const book = Book.build({
+      title: "Test Title",
+      author: "Test Author",
+      tome: 1,
+      price: 10.99,
+      publishedDate: new Date(),
+      description: "This is a valid description.",
+      popularity: 5,
+      stock: 10,
+      thumbnailImagePath: "/uploads/thumbail/test-image.jpg",
+      coverImagePath: "",
+    });
+
+    await expect(book.validate()).rejects.toThrow(
+      "Cover image path cannot be empty."
     );
   });
 });
